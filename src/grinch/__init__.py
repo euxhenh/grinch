7e9c6be97b654c845906ee0f414f9a3618a743f4
Euxhen Hasanaj <ehasanaj@cs.cmu.edu>
2022-07-19 23:18:41 -0700

    Switching to pytest for better parametrization (#2)
    Switching from unittest to pytest. Replacing custom `parametrize` with pytest's `pytest.mark.parametrize`.

--- src/grinch/__init__.py
+++ src/grinch/__init__.py
@@ -1,13 +1,23 @@
-from .aliases import ADK, OBS, VAR, AnnDataKeys
+from .aliases import ADK, OBS, OBSM, VAR, AnnDataKeys
 from .conf import BaseConfig, BaseConfigurable
+from .filters import FilterCells, FilterGenes
 from .main import instantiate_config
+from .reporter import Report, Reporter
+from .transforms import Log1P, NormalizeTotal
 
 __all__ = [
     'ADK',
     'OBS',
+    'OBSM',
     'VAR',
     'AnnDataKeys',
     'BaseConfig',
     'BaseConfigurable',
+    'FilterCells',
+    'FilterGenes',
+    'Report',
+    'Reporter',
+    'Log1P',
+    'NormalizeTotal',
     'instantiate_config',
 ]
