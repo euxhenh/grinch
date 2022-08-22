b9cc2fa12f3e4bd4499c78878b9d31e56f1eee6b
euxhenh <ehasanaj@cs.cmu.edu>
2022-08-22 10:11:16 -0400

    Minor changes to ttest

--- src/grinch/de.py
+++ src/grinch/de.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 import numpy as np
 from anndata import AnnData
+from pydantic import validator
 from sklearn.utils import indexable
 from statsmodels.stats.multitest import multipletests
 
@@ -30,26 +31,37 @@ def _correct(pvals, method='fdr_bh'):
 
 @dataclass
 class TestSummary:
-    pvals: Optional[np.ndarray] = field(default=None)
-    qvals: Optional[np.ndarray] = field(default=None)
+    pvals: np.ndarray
+    qvals: np.ndarray
     # Group means
     mean1: Optional[np.ndarray] = field(default=None)
     mean2: Optional[np.ndarray] = field(default=None)
     log2fc: Optional[np.ndarray] = field(default=None)
-    # Number of points in each group
-    n1: Optional[int] = field(default=None)
-    n2: Optional[int] = field(default=None)
 
     def __post_init__(self):
-        """Init qvals using a default correction of fdr_bg if qvals not
+        """Init qvals using a default correction of fdr_bg if qvals is not
         passed explicitly.
         """
         if self.qvals is None:
             self.qvals = _correct(self.pvals)[1]
+        if self.mean1 is None:
+            self.mean1 = np.full_like(self.pvals, np.nan)
+        if self.mean2 is None:
+            self.mean2 = np.full_like(self.pvals, np.nan)
+        if self.log2fc is None:
+            self.log2fc = np.full_like(self.pvals, np.nan)
 
-    def to_array(self):
+    def __array__(self, dtype=None):
+        """Allows np.array or np.asarray to convert this dataclass to the
+        appropriate container."""
+        return self.to_array(dtype=dtype)
+
+    def to_array(self, dtype=None) -> np.ndarray:
+        """Stacks all numeric vectors in the dataclass and returns an array
+        where rows are the tests performed.
+        """
         to_stack = [self.pvals, self.qvals, self.log2fc, self.mean1, self.mean2]
-        return np.vstack(to_stack).T
+        return np.vstack(to_stack).T.astype(dtype)
 
 
 class TTest(BaseProcessor):
@@ -62,8 +74,21 @@ class TTest(BaseProcessor):
         is_logged: bool = False
         # If the data is logged, this should point to the base of the
         # logarithm used.
-        base: Optional[str | float] = 'e'
+        base: Optional[float | str] = 'e'
         correction: str = 'fdr_bh'
+
+        @validator('base')
+        def valid_base(cls, base):
+            """Make sure the base is valid. Accepted are 'e', or any
+            positive float.
+            """
+            if isinstance(base, str):
+                if base != 'e':
+                    raise ValueError(f"Found invalid base '{base}'.")
+                return base
+            if base <= 0:
+                raise ValueError("Negative bases are not allowed.")
+            return base
 
     cfg: Config
 
@@ -86,13 +111,13 @@ class TTest(BaseProcessor):
             mean2 = np.ravel(x2.mean(axis=0))
 
             _, pvals = ttest(x1, x2)
-            qvals = _correct(pvals, method=self.cfg.correction)[1]
+            _, qvals = _correct(pvals, method=self.cfg.correction)
 
             if self.cfg.is_logged:
                 log2fc = mean1 - mean2
                 # Convert base
                 if self.cfg.base is not None and self.cfg.base != 2:
-                    base = np.e if self.cfg.base == 'e' else int(self.cfg.base)
+                    base = np.e if self.cfg.base == 'e' else float(self.cfg.base)
                     log2fc *= np.log2(base)
             else:
                 log2fc = np.log2((mean1 + 1) / (mean2 + 1))
