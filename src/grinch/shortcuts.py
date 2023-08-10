d65459ee0e2048b8ace09d47d6d35fc9e33228cf
Euxhen Hasanaj <ehasanaj@cs.cmu.edu>
2023-08-10 15:24:52 -0400

    Cleaning up and refactoring conditional filter (#101)

--- src/grinch/shortcuts.py
+++ src/grinch/shortcuts.py
@@ -4,19 +4,19 @@ from functools import partial
 
 from .cond_filter import Filter
 
-pVal_Filter_01 = partial(Filter, key='pvals', le=0.01)
-pVal_Filter_05 = partial(Filter, key='pvals', le=0.05)
-qVal_Filter_01 = partial(Filter, key='qvals', le=0.01)
-qVal_Filter_05 = partial(Filter, key='qvals', le=0.05)
-log2fc_Filter_1 = partial(Filter, key='log2fc', ge=1)
-log2fc_Filter_m1 = partial(Filter, key='log2fc', le=-1)
-log2fc_Filter_2 = partial(Filter, key='log2fc', ge=2)
-log2fc_Filter_m2 = partial(Filter, key='log2fc', le=-2)
-abs_log2fc_Filter_1 = partial(Filter, key='abs_log2fc', ge=1)
-abs_log2fc_Filter_2 = partial(Filter, key='abs_log2fc', ge=2)
+pVal_Filter_01 = partial(Filter[float], key='pvals', le=0.01)
+pVal_Filter_05 = partial(Filter[float], key='pvals', le=0.05)
+qVal_Filter_01 = partial(Filter[float], key='qvals', le=0.01)
+qVal_Filter_05 = partial(Filter[float], key='qvals', le=0.05)
+log2fc_Filter_1 = partial(Filter[float], key='log2fc', ge=1)
+log2fc_Filter_m1 = partial(Filter[float], key='log2fc', le=-1)
+log2fc_Filter_2 = partial(Filter[float], key='log2fc', ge=2)
+log2fc_Filter_m2 = partial(Filter[float], key='log2fc', le=-2)
+abs_log2fc_Filter_1 = partial(Filter[float], key='abs_log2fc', ge=1)
+abs_log2fc_Filter_2 = partial(Filter[float], key='abs_log2fc', ge=2)
 # For lead gene discovery in a GSEA prerank test
-FDRqVal_Filter_05 = partial(Filter, key='FDR q-val', le=0.05)
-FWERpVal_Filter_05 = partial(Filter, key='FWER p-val', le=0.05)
+FDRqVal_Filter_05 = partial(Filter[float], key='FDR q-val', le=0.05)
+FWERpVal_Filter_05 = partial(Filter[float], key='FWER p-val', le=0.05)
 
 __all__ = [
     'pVal_Filter_01',
